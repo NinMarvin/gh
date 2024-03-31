b860b1fcfb6a14e215265bb3e5026aca693aef2e
Marvin <64258828+NinMarvin@users.noreply.github.com>
2024-03-31 15:03:58 +0200

    Update password.py

--- password.py
+++ password.py
@@ -4,7 +4,7 @@ import random, string  # Hinzufügen von random und string
 s=int(input('Stellen des Passworts='))  # Wie viel stellen soll das Passwort haben s(Stellen)
 print('')  # Eine Leere Zeile augeben
 
-p=[]  # Liste5 Passwort erstellt
+p=[]  # Liste5 Passwort erstelltl
 #und
 for i in range(s):  # Die Schleife soll so oft durchlaufen wiegg bei s(Stellen) gewählt
     r=random.choice(string.ascii_letters + string.digits)  # Es werden zufällig string.ascii_letters(groß und klein Buchstaben) und string.digits(Zahlen) erstellt
